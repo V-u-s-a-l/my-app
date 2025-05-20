929b9e25454404b1d7f5cf1d0f184f75565ed93e
Vusal Bayramli <bayramliv7@gmail.com>
2025-05-20 23:01:01 +0400

    hello

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import './App.css';
 import RestaurantCard from './RestaurantCard';
 import Restaurant from './image/restaurant.avif';
+import Restaurant2 from './image/restaurant2.avif';
+import Restaurant3 from './image/restaurant3.avif';
 
 
 
@@ -13,6 +15,41 @@ function App() {
       imageUrl:Restaurant,
       rating:4,
       cuisine:"Azerbaycan metbexi"
+    },
+     {
+      id:2,
+      name:"Lorem ipsum dolor",
+      imageUrl:Restaurant2,
+      rating:3.5,
+      cuisine:"Turk metbexi"
+    },
+     {
+      id:3,
+      name:"Lorem ipsum dolor",
+      imageUrl:Restaurant3,
+      rating:4.5,
+      cuisine:"Rus metbexi"
+    },
+    {
+      id:4,
+      name:"Lorem ipsum dolor",
+      imageUrl:Restaurant,
+      rating:3,
+      cuisine:"Azerbaycan metbexi"
+    },
+     {
+      id:5,
+      name:"Lorem ipsum dolor",
+      imageUrl:Restaurant2,
+      rating:2.5,
+      cuisine:"Turk metbexi"
+    },
+     {
+      id:6,
+      name:"Lorem ipsum dolor",
+      imageUrl:Restaurant3,
+      rating:5,
+      cuisine:"Rus metbexi"
     }
   ])
   return (
